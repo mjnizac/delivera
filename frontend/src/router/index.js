3b83c9ec4b212ade589c2be9ce6b493740021601
Manuel.J Niza Cobo <mannizcob@alum.us.es>
2026-02-08 15:18:34 +0100

    feat - Nuevo path para registro creado

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: LoginView },
+    { path: '/register', component: RegisterView },
   ],
 })
 
